6c7af932e462d5fd368c235e6294d365f170ae83
Fuzzkatt <zonghan2000@gmail.com>
2018-02-01 10:57:51 -0800

    working blob detection for yellow sticky notes

---
+++ Robot.h
@@ -0,0 +1,27 @@
+//#define _GLIBCXX_USE_CXX11_ABI 0
+#ifndef ROBOT_H
+#define ROBOT_H
+
+#include <Commands/Command.h>
+#include <IterativeRobot.h>
+#include <LiveWindow/LiveWindow.h>
+
+#include <OI.h>
+#include "Commands/TestPipeline.h"
+#include "WPILib.h"
+
+class Robot: public frc::IterativeRobot{
+public:
+
+private:
+	frc::LiveWindow* lw = frc::LiveWindow::GetInstance();
+
+	void RobotInit() override;
+	void AutonomousInit() override;
+	void AutonomousPeriodic() override;
+	void TeleopInit() override;
+	void TeleopPeriodic() override;
+	void TestPeriodic() override;
+};
+
+#endif  // OI_H
